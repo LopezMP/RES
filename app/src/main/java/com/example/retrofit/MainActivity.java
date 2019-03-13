1c8822fa2d014acea298a637330dad52e265da81
LopezMP <LopezMP@users.noreply.github.com>
2019-03-13 20:54:25 +0100

    faltas cosas

--- app/src/main/java/com/example/retrofit/MainActivity.java
+++ app/src/main/java/com/example/retrofit/MainActivity.java
@@ -2,14 +2,76 @@ package com.example.retrofit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.List;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+import android.app.Activity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import java.util.ArrayList;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        recyclerView = findViewById(R.id.my_recycler_view);
+        // use this setting to
+        // improve performance if you know that changes
+        // in content do not change the layout size
+        // of the RecyclerView
+        recyclerView.setHasFixedSize(true);
+        // use a linear layout manager
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        List<String> input = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            input.add("Test1" + i);
+        }// define an adapter
+        mAdapter = new MyAdapter(input);
+        recyclerView.setAdapter(mAdapter);
+
+
+/*
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                GitHubService gitHubService = GitHubService.retrofit.create(GitHubService.class);
+                final Call<List<Contributor>> call =
+                        gitHubService.repoContributors("square", "retrofit");
+
+                call.enqueue(new Callback<List<Contributor>>() {
+                    @Override
+                    public void onResponse(Call<List<Contributor>> call, Response<List<Contributor>> response) {
+                        final TextView textView = findViewById(R.id.textView);
+                        textView.setText(response.body().toString());
+                    }
+
+                    @Override
+                    public void onFailure(Call<List<Contributor>> call, Throwable t) {
+                        final TextView textView = findViewById(R.id.textView);
+                        textView.setText("Something went wrong: " + t.getMessage());
+                    }
+                });
+            }
+        }); */
+
     }
+
+
+
 }
